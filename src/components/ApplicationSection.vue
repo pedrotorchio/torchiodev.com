d83789a6de0d1e99e7401ea4828496bc9354f31f
Pedro Torchio <pedrotorchio@gmail.com>
2019-03-14 19:20:05 -0300

    initial title and screens

---
+++ src/components/ApplicationSection.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    section#application-section.page-section.padded
+        .app-section
+            .screen-1.mdc-elevation--z2
+            .screen-2.mdc-elevation--z2
+            .phone
+        .title-group
+        
+            h2.block-title
+                span.line.big Transform
+                span.line.big Your Work
+                span.line.big Routine.
+                span.line.medium Make Life Easier.
+            p.title-text
+                | It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+</template>
+<style lang="sass" scoped>
+#application-section
+    $bg-blue: #f2f6ff
+    background: radial-gradient(circle 60vw at left bottom, white 0%, #{$bg-blue} 100%)
+    display: flex
+    align-items: center
+
+.app-section
+    flex: 1 1 auto
+.title-group
+    flex: 0 1 auto
+    
+.screen-1, .screen-2
+    margin: 50px auto
+    $w: 500px
+    width: $w
+    height: $w * .5
+    background: white
+.title-text
+    width: 450px
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   div#home-page
     corporate-section
     ecommerce-section
+    application-section
 </template>
 
 <script>
 import CorporateSection from '@/components/CorporateSection'
 import EcommerceSection from '@/components/EcommerceSection'
+import ApplicationSection from '@/components/ApplicationSection'
 
 export default {
   name: 'home',
   components: {
     CorporateSection,
     EcommerceSection,
+    ApplicationSection,
   }
 }
 </script>
