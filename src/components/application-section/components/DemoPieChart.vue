28bf3d5aaa2ced6b3ee9f8afb9d2d42d3a588e40
Pedro Torchio <pedrotorchio@gmail.com>
2019-03-16 19:43:38 -0300

    d3 struggle

--- src/components/application-section/components/DemoPieChart.vue
+++ src/components/application-section/components/DemoPieChart.vue
@@ -1,115 +1,160 @@
 <template lang="pug">
     article#demo-pie-chart
         svg( viewport="" :width="width" :height="height")
-            g( ref="chart-group" :transform="`translate(${width/2}, ${height/2})`")
+          g.chart-group( :transform="`translate(${width/2}, ${height/2})`")
+            
 </template>
 
 <script>
-import { getGenderUidByLabel, getGenderLabelAndColor } from '../CustomerDataset'
+import {
+  getGenderUidByLabel,
+  getGenderLabelAndColor
+} from "../CustomerDataset";
 
-import { arc as d3Arc, pie as d3Pie } from 'd3-shape'
-import { select as d3Select } from 'd3-selection'
-import { interpolate as d3Interpolate } from 'd3-interpolate'
-import { easeElasticOut as d3Easing } from 'd3-ease'
+import { arc as d3Arc, pie as d3Pie } from "d3-shape";
+import { select as d3Select } from "d3-selection";
+import { interpolate as d3Interpolate } from "d3-interpolate";
+import { easeQuadOut as d3Easing } from "d3-ease";
 
-import 'd3-transition'
+import "d3-transition";
 
-
+const selectors = {
+  centerG: ".chart-group",
+  arcGs: ".arc-g"
+};
 export default {
-    props: {
-        customers: Array,
-        size: Number
+  props: {
+    customers: Array,
+    size: Number
+  },
+  computed: {
+    groupedGenders() {
+      return this.customers.reduce(
+        (partitions, customer) => {
+          partitions[customer.gender].count++;
+          return partitions;
+        },
+        [
+          { gender: 0, count: 0 },
+          { gender: 1, count: 0 },
+          { gender: 2, count: 0 }
+        ]
+      );
     },
-    computed: {
-        groupedGenders() {
-            return this.customers
-            .reduce((partitions, customer) => {
-
-                partitions[customer.gender].count++;
-                return partitions
-
-            }, [{ gender: 0, count: 0 }, { gender: 1, count: 0 }, { gender: 2, count: 0 }])
-        },
-        arcGenerator(){
-            return d3Arc()
-                .outerRadius(this.radius)
-                .innerRadius(50)
-        },
-        pieGenerator() {
-            return d3Pie()
-                .value(d => d.count)
-        },
-        width() {
-            return this.size
-        },
-        height() {
-            return this.size
-        },
-        radius() {
-            return this.size / 3
-        }
+    arcGenerator() {
+      return d3Arc()
+        .outerRadius(this.radius)
+        .innerRadius(50);
     },
-    methods: {
-        enterData(arcs) {
-            const
-                // Gs 
-                enterArcs = arcs.enter()
-                    .append("g")
-                    .each(function(d) {
-                        this.classList.add(`arc`)
-                        this.classList.add(`${getGenderLabelAndColor(d.data.gender).label}-${d.data.gender}`)
-                        this.classList.add(`count-${d.data.count}`)
-                    }),
-
-                // PATHs
-                enterPaths = enterArcs                
-                    .append("path")
-                    .style("fill", c => getGenderLabelAndColor(c.data.gender).color ),
-
-                // TEXTs
-                enterTexts = enterArcs
-                    .append("text")
-                    .attr("transform", d => `translate(${this.arcGenerator.centroid(d)})`)
-                    .attr("text-anchor", "middle")
-                    .attr("dominant-baseline", "central")
-                    .attr("fill", "white")     
-                    .attr("font-size", "24px")
-                    .text(d => getGenderLabelAndColor(d.data.gender).label.charAt(0));
-                
-                // ANIMATION
-                enterPaths
-                    .transition()
-                            .ease(d3Easing)
-                            .duration(d => (d.endAngle - d.startAngle) * 1500)
-                            .delay((d,i) => i * 150)
-                        .attrTween("d", b => {
-                            const i = d3Interpolate({ startAngle: b.startAngle, endAngle: b.startAngle }, b);
-                            return t => this.arcGenerator(i(t))
-                        })
-
-                return enterArcs
-        },
-        updateChart() {
-            console.log(this.pieGenerator(this.groupedGenders));
-            const 
-                chartGroup = d3Select(this.$refs["chart-group"]),
-                arcs = chartGroup.selectAll('.arc')
-                    .data(this.pieGenerator(this.groupedGenders), d => d.data.gender),
-                
-                enterArcs = this.enterData(arcs)
-        }
+    pieGenerator() {
+      return d3Pie()
+        .value(d => d.count)
+        .sort(d => d.gender);
     },
-    mounted() {
-        this.updateChart()
+    width() {
+      return this.size;
     },
-    watch: {
-        groupedGenders: {
-            deep: true,
-            handler() {
-                console.log("changed")
-                this.updateChart()
-            }
-        }
+    height() {
+      return this.size;
+    },
+    radius() {
+      return this.size / 3;
     }
-}
+  },
+  methods: {
+    applyTransition(selection) {
+      selection = selection
+        .transition()
+        .ease(d3Easing)
+        .duration(d => (d.endAngle - d.startAngle) * 200)
+        .delay((d, i) => i * 150);
+
+      return selection;
+    },
+    animateText(text, isInitial = false) {
+      text.style("opacity", 0);
+      this.applyTransition(text)
+        .attr("transform", d => `translate(${this.arcGenerator.centroid(d)})`)
+        .style("opacity", 1);
+    },
+    animatePath(paths, isInitial = false) {
+      this.applyTransition(paths).attrTween("d", b => {
+        const i = d3Interpolate(
+          { startAngle: b.startAngle, endAngle: b.startAngle },
+          b
+        );
+        return t => this.arcGenerator(i(t));
+      });
+    },
+    enterData(arcs) {
+    
+      const // Create new Gs and Paths and color them
+        enterGs = arcs.enter().append("g")
+          .classed('new', true)
+          .classed(selectors.arcGs, true)
+          .each(function(d) {
+            this.classList.add(`${getGenderLabelAndColor(d.data.gender).label}-${d.data.gender}`)
+            this.classList.add(`count-${d.data.count}`)
+          }),
+
+        enterPaths = enterGs
+          .append("path")
+          .style("fill", c => getGenderLabelAndColor(c.data.gender).color),
+        enterText = enterGs
+          .append("text")
+          .attr("transform", d => `translate(${this.arcGenerator.centroid(d)})`)
+          .attr("text-anchor", "middle")
+          .attr("dominant-baseline", "central")
+          .attr("fill", "white")
+          .attr("font-size", "18px")
+          .text(
+            d => getGenderLabelAndColor(d.data.gender).label.charAt(0) + d.value
+          ),
+        allPaths = enterPaths
+          .merge(arcs.selectAll('path'))
+
+
+      // animate value
+      this.animatePath(allPaths);
+      this.animateText(enterText);
+    },
+    updateData(arcs) {
+      
+      arcs.classed('old', true)
+      arcs.classed('new', false)
+    },
+    exitData(arcs) {
+      arcs.remove();
+    },
+    joinDataAndArcs() {
+        const data = this.pieGenerator(this.groupedGenders);
+        console.log(`Joining ${data.length} items`)
+
+        const chartGroup = d3Select(selectors.centerG),
+        arcs = chartGroup
+            .selectAll(selectors.arcGs)
+            .data(data, d => d.data.gender);
+
+        return arcs;
+    },
+    updateChart() {
+        const arcs = this.joinDataAndArcs();
+
+        this.updateData(arcs);
+        this.enterData(arcs);
+        this.exitData(arcs.exit());
+    },
+  },
+  mounted() {
+    this.updateChart()
+  },
+  watch: {
+    groupedGenders: {
+      deep: true,
+      handler(val) {
+        this.updateChart();
+      }
+    }
+  }
+};
 </script>
