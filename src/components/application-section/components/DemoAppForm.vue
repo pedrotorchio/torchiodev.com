42526bdc2e4320dffd70110681667f7d31d8db0f
Pedro Torchio <pedrotorchio@gmail.com>
2019-03-22 14:48:57 -0300

    fixing app layout

--- src/components/application-section/components/DemoAppForm.vue
+++ src/components/application-section/components/DemoAppForm.vue
@@ -24,7 +24,6 @@
                     input#demo-age( type="number" name="age" v-model.number="customer.age" min="10" max="100" step="5")
         .actions
             button.mdc-elevation--z2.green( :disabled="!done" @click="$emit('save', customer)" ) Save
-            button.mdc-elevation--z2.red( @click="$emit('return')" ) X
 
         //- label( for="demo-satisfaction" ) Satisfaction Level
         //- input#demo-satisfaction( type="range" min="0" max="100" step="5" placeholder="Full Name" )
