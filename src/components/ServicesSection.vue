ddbb25e3ad1cbc8a14544cda87ce531b8c9e037a
Pedro Torchio <pedrotorchio@gmail.com>
2019-05-04 17:55:09 +1000

    toggle services

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -3,14 +3,14 @@
         .inner-section
             ul.services
                 li( v-for="(service, i) in services" :key="i")
-                    service-item( :service="service" :toggled="toggledService == i" :invert="i > 1" )
-                    //- @toggle="open(i)"
-                    //- @hover="open(i)" 
-                    //- @close="close(i)" )
+                    service-item( :service="service" :toggled="toggledService == i" :invert="i > 1"
+                    @toggle="open(i)"
+                    @close="close(i)" )
 </template>
 <script>
 import services from '@/assets/services'
 import ServiceItem from './services-section/ServiceItem'
+import { rMd } from 'media-query-mixins/_mixins'
 
 export default {
     components: { ServiceItem },
@@ -20,11 +20,13 @@ export default {
     }),
     methods: {
         open(i) {
-            this.toggledService = i
+            rMd(()=>this.toggledService = i);
         },
         close(i) {
-            if (this.toggledService == i)
-                this.toggledService = -1
+            rMd(()=>{
+                if (this.toggledService == i)
+                    this.toggledService = -1
+            });
         }
     }
 }
